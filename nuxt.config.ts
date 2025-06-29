845b29eabdefcc94329c05a7aec4d3e477c3cfe9
BroneKot <13498924+bronekot@users.noreply.github.com>
2025-06-29 14:24:13 +0300

    fix: update language attribute from 'en' to 'ru' in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
     head: {
       title: 'Phasmophobia Helper',
       htmlAttrs: {
-        lang: 'en',
+        lang: 'ru',
       },
       link: [
         {
